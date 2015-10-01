8f290c069e82e6edeff208eb3d52043eddee19e9
Melissa W <mwebdesign@icloud.com>
2015-10-01 12:55:51 -0400

    Setting up project

---
+++ WhackAMole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhackAMole
+//
+//  Created by Melissa on 10/1/15.
+//  Copyright © 2015 Melissa Webster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
